132574dc0ce36394f64af8a5d844e9410e80a124
ebrightmoon <jinsedeyuzhou@sina.com>
2018-10-30 19:56:58 +0800

    shiro

---
+++ ebrightmoon-manager/ebrightmoon-manager-controller/src/main/java/com/ebrightmoon/manager/realm/LoginRealm.java
@@ -0,0 +1,29 @@
+package com.ebrightmoon.manager.realm;
+
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
+
+
+
+
+@Component
+public class LoginRealm extends AuthorizingRealm{
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+  
+}
